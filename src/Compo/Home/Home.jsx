7c5a332c8d3550ca32fbdee0b9a49f572df09691
sakibsarkar <sakibsarkar707@gmail.com>
2023-10-22 18:32:25 +0600

    rigt side bar added

--- src/Compo/Home/Home.jsx
+++ src/Compo/Home/Home.jsx
@@ -1,9 +1,11 @@
 import SideBar_left from "../Sidebar_left/Sidebar_left";
+import SideBar_right from "../SideBar_right/SideBar_right";
 
 const Home = () => {
     return (
         <>
             <SideBar_left></SideBar_left>
+            <SideBar_right></SideBar_right>
         </>
     );
 };
--- src/Compo/SideBar_right/SideBar_right.jsx
+++ src/Compo/SideBar_right/SideBar_right.jsx
@@ -1,7 +1,45 @@
+import "./SideBar_right.css";
+
 const SideBar_right = () => {
     return (
-        <div>
-            
+        <div className="rightSideBar">
+
+
+
+            <div className="sponsored">
+                <h1>Sponsored</h1>
+
+                <div className="sponsoredContent">
+
+                    <img src="https://pathao.com/np/wp-content/uploads/sites/7/2018/12/Download-app.jpg" />
+                    <div>
+                        <h1>Ride on uthao</h1>
+                        <p>www.uthao.bd.com</p>
+                    </div>
+                </div>
+            </div>
+
+
+            <div className="recentReq">
+                <h1>Friend Request</h1>
+
+                <a href="https://www.facebook.com/me.facebook.id" target="_blank" rel="noreferrer" >
+
+                    <img src="https://avatars.githubusercontent.com/u/119102204?v=4" />
+
+                    <div className="requestedId">
+                        <h1>Najmus Sakib</h1>
+                        <div className="reqBtns">
+                            <button className="confirm">Confirm</button>
+                            <button className="delete">Delete</button>
+                        </div>
+                    </div>
+
+                </a>
+
+            </div>
+
+
         </div>
     );
 };
